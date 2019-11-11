730968b22e734cf405874cf80d62eacf3a056e9c
Papan Das <hexoindia@gmail.com>
2019-11-11 18:47:15 +0530

    TimeZoneConvertion Fixes

--- docs/js/subscription_app.js
+++ docs/js/subscription_app.js
@@ -17,6 +17,25 @@ App = {
   init: function () {
     
     console.log("App initialized...", "TimeInMilliseconds", App.ReturnUTCTime(), "--", App.FormatDateTime(App.ReturnUTCTime()))
+
+    // Multiply by 1000 because JS works in milliseconds instead of the UNIX seconds
+    var date = new Date();
+
+    var year = date.getUTCFullYear();
+    var month = date.getUTCMonth() + 1; // getMonth() is zero-indexed, so we'll increment to get the correct month number
+    var day = date.getUTCDate();
+    var hours = date.getUTCHours();
+    var minutes = date.getUTCMinutes();
+    var seconds = date.getUTCSeconds();
+
+    month = (month < 10) ? '0' + month : month;
+    day = (day < 10) ? '0' + day : day;
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds: seconds;
+
+    console.log(year + '-' + month + '-' + day + ' ' + hours + ':' + minutes);
+    
     return App.initWeb3();
   },
 
