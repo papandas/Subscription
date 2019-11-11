0f991d7102eabd72712bbe35a534a07d0ede3805
Papan Das <hexoindia@gmail.com>
2019-11-11 12:17:42 +0530

    Time countdown fixing

--- docs/js/subscription_app.js
+++ docs/js/subscription_app.js
@@ -323,14 +323,14 @@ App = {
   FormatDateTime: function (_date) {
     //console.log(_date, typeof(_date))
     let date = new Date(_date);
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
+    var hours = date.getUTCHours();
+    var minutes = date.getUTCMinutes();
     var ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0' + minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
-    return date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+    return date.getUTCMonth() + 1 + "/" + date.getUTCDate() + "/" + date.getUTCFullYear() + "  " + strTime;
   },
 
   ReturnUTCTime: function () {
