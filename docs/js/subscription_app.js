3e605d06f14641d650a3a5750ac423b50c5bdb69
Papan Das <hexoindia@gmail.com>
2019-11-11 22:12:34 +0530

    TimeZoneConvertion Fixes

--- docs/js/subscription_app.js
+++ docs/js/subscription_app.js
@@ -207,7 +207,7 @@ App = {
               console.log(i, subscribe[1], "TimeImMilliSeconds", subscriberDt , "Time(EVM)", App.FormatDateTime(subscriberDt), "-NowInGMT-", App.FormatDateTime(utcTime) );
               let dt = new Date();
               let evmDt = new Date(subscriberDt+(dt.getTimezoneOffset()*60*1000)-(parseInt(offset)*60*1000) );
-              console.log("[::Verify::]", evmDt, App.FormatDateTime(evmDt.toUTCString()), dt.getTimezoneOffset()), (utcTime <= evmDt);
+              console.log("[::Verify::]", evmDt.getTime(), App.FormatDateTime(evmDt.toUTCString()), dt.getTimezoneOffset()), (utcTime <= evmDt.getTime());
               console.log("[::Verify::] Offset", offset)
 
               //var results = Math.round(subscriberDt - dt.getTimezoneOffset()*60*1000);
